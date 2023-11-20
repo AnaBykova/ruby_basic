ff594df344219861db7e2e95c5a46f56f8082932
AnaBykova <anabykovaca@gmail.com>
2023-11-19 19:01:06 -0700

    ex 5 - 11

---
+++ ex11.rb
@@ -0,0 +1,7 @@
+print "How old are you? "
+age = gets.chomp
+
+print "How tall are you? "
+height = gets.chomp
+
+print "So you are #{age} years old and #{height} tall."
---
+++ ex5.rb
@@ -0,0 +1,27 @@
+my_name = "Ana B"
+my_age = 100
+my_height = 74
+
+puts "Lets's talk about #{my_name}."
+puts "She is #{my_age} years old and #{my_height} inches tall."
+
+puts "If I add #{my_age} and #{my_height} I get #{my_age + my_height}."
+puts "_________________"
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts "_________________"
+
