d1ed83ecbe0c22793561a7dbd8a53a8b7f85c1c3
AnaBykova <anabykovaca@gmail.com>
2023-11-19 18:43:54 -0700

    exercise 1 - 4

---
+++ ex1.rb
@@ -0,0 +1,13 @@
+puts "Hello Worrld!";
+puts "Hello Again";
+#test comments
+puts "Test comments2"
+
+puts "Math test";
+puts "Hens #{25 + 30 / 6}";
+puts "Roosters #{100 - 25 * 3 % 4}";
+puts 3 + 2 < 5 - 7;
+
+puts "Varibables";
+cars = 100;
+puts "There are #{cars} cars available";
